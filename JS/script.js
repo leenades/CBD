7cb9379d789fd3e4f20ba4968f4cb135e12a85f4
leenades <57545008+leenades@users.noreply.github.com>
2021-10-18 15:48:03 +0400

    commit 18 oct

--- JS/script.js
+++ JS/script.js
@@ -52,16 +52,32 @@ input.addEventListener("keyup", reset);
 
 $(document).ready(function () {
 
-  // var counterNumber = document.querySelectorAll('.counter-numbers');
-  // var additionOp = document.querySelectorAll('.additionOp');
 
-  // for(var i = 0; i < additionOp.length; i++){
-  //   var j = i
-  //   $(additionOp[j]).on('click', function(e){
-  //     e.preventDefault();
-      
-  //   });
-  // }
+// COUNTER FUNCTION START
+// on change value of counter one two three and four
+$('#counter-1, #counter-2, #counter-3, #counter-4').on('change', totalAmountChange);
+
+function totalAmountChange() {
+  var counterOne = $('#counter-1').val();
+  var counterTwo = $('#counter-2').val();
+  var counterThree = $('#counter-3').val();
+  var counterFour = $('#counter-4').val();
+
+  var updatedTotalAmount = (+counterOne + +counterTwo + +counterThree + +counterFour) / 4; 
+  $('#totalAmount').attr('data-value', updatedTotalAmount);
+  $('#totalAmount').html(updatedTotalAmount);
+}
+
+$('.counter-btn').on('click', function(e) {
+  e.preventDefault();
+  var button = $(this);
+  var counterValue = $('#counter-' + button.data('index')).val(); //1000.00
+  var counterIncVal = button.data('inc'); //10 or -10
+
+  $('#counter-' + button.data('index')).val( +counterValue + +counterIncVal);
+  totalAmountChange();
+});
+// COUNTER FUNCTION END
 
 
 
@@ -80,9 +96,7 @@ $(document).ready(function () {
 
 
 
-  if ($("#darkSwitch").hasClass("checked")) {
-    $("#darkSwitch").removeClass("checked");
-  }
+
   $(".js-products-slider-styles").slick({
     dots: true,
     arrows: true,
@@ -135,12 +149,36 @@ $(document).ready(function () {
       },
     ],
   });
+  $(".js-slider-distinct").slick({
+    dots: true,
+    arrows: true,
+    infinite: false,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    appendArrows: $("#arrowsAppendDistinct"),
+    variableWidth: true,
+    lazyLoad: "ondemand",
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          centerPadding: '32px',
+        },
+      },
+    ],
+  });
+  if ($("#darkSwitch").hasClass("checked")) {
+    $("#darkSwitch").removeClass("checked");
+  }
+
   $("#darkSwitch").on("click", function () {
     $("#darkSwitch").toggleClass("checked");
   });
 
 
-  $('body').on("scroll", function () {
+  $('body').on("scroll resize", function () {
     var windowTop = $(window).scrollTop();
     var windowHeight = $(window).height();
     var windowBottomPos = windowTop + windowHeight;
