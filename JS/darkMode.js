77895f71efa2369fa9b10bf897e01a810c4f48db
leenades <57545008+leenades@users.noreply.github.com>
2022-01-20 15:29:25 +0400

    commit changes

--- JS/darkMode.js
+++ JS/darkMode.js
@@ -2,18 +2,27 @@ $(document).ready(function () {
   $("#darkSwitch").on("click", function () {
     $("#darkSwitch").toggleClass("checked");
     if ($("#darkSwitch").hasClass("checked")) {
+      $('.light-icon').removeClass('d-none');
+      $('.dark-icon').addClass('d-none');
       $(".addLightBG").addClass("lightback-ground");
       $(".btn-white").addClass("btn-white-dark-styles");
       $(".btndark-transparent-bg").addClass(
         "btndark-transparent-bg-dark-styles"
       );
+      $('.form-control').addClass('form-dark');
+      $('.label').addClass('form-dark');
       $(".products-header-title").addClass("products-header-title-dark-styles");
       $(".whiteFontDark").addClass("white-font");
+      $(".greyFontDark").addClass("grey-font");
       $(".whiteFontDarkSymbol, .arrowsDark").addClass("white-font");
-      $(".dropdown-menu, .dropdown-menu.secondary-nav-inner-dropdowns, .card , .arrow, .accordion-item, .blurb, .distinct-slider-container").addClass("dark-style-bg");
+      $(".dropdown-menu, .dropdown-menu.secondary-nav-inner-dropdowns, .card , .arrow, .accordion-item, .blurb, .distinct-slider-container, .mobile-menu, .top-features-box, .form-styles").addClass("dark-style-bg");
       $('.darkModeBG').addClass('dark-styles');
       $('.btn-blue').addClass("darkFont");
     } else {
+      $('.form-control').removeClass('form-dark');
+      $('.label').removeClass('form-dark');
+      $('.light-icon').addClass('d-none');
+      $('.dark-icon').removeClass('d-none');
       $(".addLightBG").removeClass("lightback-ground");
       $(".btn-white").removeClass("btn-white-dark-styles");
       $(".btndark-transparent-bg").removeClass(
@@ -23,11 +32,11 @@ $(document).ready(function () {
         "products-header-title-dark-styles"
       );
       $(".whiteFontDark").removeClass("white-font");
+      $(".greyFontDark").removeClass("grey-font");
       $(".whiteFontDarkSymbol, .arrowsDark").removeClass("white-font");
-      $(".dropdown-menu, .dropdown-menu.secondary-nav-inner-dropdowns, .card , .arrow, .accordion-item, .blurb, .distinct-slider-container").removeClass("dark-style-bg");
+      $(".dropdown-menu, .dropdown-menu.secondary-nav-inner-dropdowns, .card , .arrow, .accordion-item, .blurb, .distinct-slider-container, .mobile-menu, .top-features-box, .form-styles").removeClass("dark-style-bg");
       $('.darkModeBG').removeClass('dark-styles');
       $('.btn-blue').removeClass("darkFont");
-
     }
   });
 });
--- JS/script.js
+++ JS/script.js
@@ -1,14 +1,52 @@
 // AOS.init();
 
-
 $(document).ready(function () {
+  $(".back-to-styles").on("click", function () {
+    $(this).addClass('clicked');
+    if($(this).hasClass('clicked')){
+      $(this).addClass('closed');
+      if($('ul.dropdown-menu.sub-menu02').hasClass('show') && $(this).hasClass('clicked')){
+        $('.dropdown-menu.sub-menu02').removeClass('show');
+        $('.dropdown.drop-down02').removeClass('show');
+      }
+      return false;
+    }
+  });
+
+  $('.primary-nav-item').on('click', function(e) {
+    e.preventDefault();
+    $(".primary-nav-item").removeClass('current-item');
+    $(this).addClass('current-item');
+  });
+
+  $('.main-mob-nav-items').on('click', function(e) {
+    e.preventDefault();
+    $(".main-mob-nav-items").removeClass('current-item');
+    $(this).addClass('current-item');
+    if($(this).hasClass('current-item')){
+      $(".main-mob-nav-items").children().removeClass('show');
+      $(this).children().addClass('show');
+    } 
+  });
+  $('.inner-sub-menu-mob-items').on('click', function(e) {
+    e.preventDefault();
+    $(".inner-sub-menu-mob-items").removeClass('current-item');
+    $(this).addClass('current-item');
+    if($(this).hasClass('current-item')){
+      $(".inner-sub-menu-mob-items").children().removeClass('show');
+      $(this).children().addClass('show');
+    } 
+  });
+  
 
   $("#item-5677").on("mouseover", function () {
     $(this).addClass("hover");
     $("#item-5677 .secondary-nav-inner-dropdowns").addClass("show");
     if ($("#item-5678, #item-5679").hasClass("hover")) {
       $("#item-5678, #item-5679").removeClass("hover");
-      $("#item-5678 .secondary-nav-inner-dropdowns, #item-5679 .secondary-nav-inner-dropdowns").removeClass("show");
+      $(
+        "#item-5678 .secondary-nav-inner-dropdowns, #item-5679 .secondary-nav-inner-dropdowns"
+      ).removeClass("show");
     }
   });
   $("#item-5678").on("mouseover", function () {
@@ -16,7 +54,9 @@ $(document).ready(function () {
     $("#item-5678 .secondary-nav-inner-dropdowns").addClass("show");
     if ($("#item-5677, #item-5679").hasClass("hover")) {
       $("#item-5677, #item-5679").removeClass("hover");
-      $("#item-5677 .secondary-nav-inner-dropdowns, #item-5679 .secondary-nav-inner-dropdowns").removeClass("show");
+      $(
+        "#item-5677 .secondary-nav-inner-dropdowns, #item-5679 .secondary-nav-inner-dropdowns"
+      ).removeClass("show");
     }
   });
   $("#item-5679").on("mouseover", function () {
@@ -24,20 +64,24 @@ $(document).ready(function () {
     $("#item-5679 .secondary-nav-inner-dropdowns").addClass("show");
     if ($("#item-5678, #item-5677").hasClass("hover")) {
       $("#item-5678, #item-5677").removeClass("hover");
-      $("#item-5678 .secondary-nav-inner-dropdowns, #item-5677 .secondary-nav-inner-dropdowns").removeClass("show");
+      $(
+        "#item-5678 .secondary-nav-inner-dropdowns, #item-5677 .secondary-nav-inner-dropdowns"
+      ).removeClass("show");
     }
   });
   $("#item-5677, #item-5678, #item-5679").on("mouseleave", function () {
-    $("#item-5678 .secondary-nav-inner-dropdowns, #item-5677 .secondary-nav-inner-dropdowns, #item-5679 .secondary-nav-inner-dropdowns").removeClass("show");
+    $(
+      "#item-5678 .secondary-nav-inner-dropdowns, #item-5677 .secondary-nav-inner-dropdowns, #item-5679 .secondary-nav-inner-dropdowns"
+    ).removeClass("show");
   });
 
-// navbar current button styles start
+  // navbar current button styles start
   $(".dropdown-inner-menu-item").click(function (e) {
     e.preventDefault();
     $(".dropdown-inner-menu-item").removeClass("current-item");
     $(this).addClass("current-item");
   });
-// navbar current button styles end
+  // navbar current button styles end
 
   $("body").on("scroll resize", function () {
     var windowTop = $(window).scrollTop();
@@ -67,4 +111,72 @@ $(document).ready(function () {
       $(".cbd-light-logo").removeClass("d-none");
     }
   });
+  (function ($bs) {
+    const CLASS_NAME = "has-child-dropdown-show";
+    $bs.Dropdown.prototype.toggle = (function (_orginal) {
+      return function () {
+        document.querySelectorAll("." + CLASS_NAME).forEach(function (e) {
+          e.classList.remove(CLASS_NAME);
+        });
+        let dd = this._element
+          .closest(".dropdown")
+          .parentNode.closest(".dropdown");
+        for (
+          ;
+          dd && dd !== document;
+          dd = dd.parentNode.closest(".dropdown")
+        ) {
+          dd.classList.add(CLASS_NAME);
+        }
+        return _orginal.call(this);
+      };
+    })($bs.Dropdown.prototype.toggle);
+
+    document.querySelectorAll(".dropdown").forEach(function (dd) {
+      dd.addEventListener("hide.bs.dropdown", function (e) {
+        if (this.classList.contains(CLASS_NAME)) {
+          this.classList.remove(CLASS_NAME);
+          e.preventDefault();
+        }
+        if (
+          e.clickEvent &&
+          e.clickEvent
+            .composedPath()
+            .some(
+              (el) =>
+                el.classList && el.classList.contains("dropdown-toggle")
+            )
+        ) {
+          e.preventDefault();
+        }
+        e.stopPropagation(); // do not need pop in multi level mode
+      });
+    });
+
+    // for hover
+    function getDropdown(element) {
+      return $bs.Dropdown.getInstance(element) || new $bs.Dropdown(element);
+    }
+
+    document
+      .querySelectorAll(".dropdown-hover, .dropdown-hover-all .dropdown")
+      .forEach(function (dd) {
+        dd.addEventListener("mouseenter", function (e) {
+          let toggle = e.target.querySelector(
+            ':scope>[data-bs-toggle="dropdown"]'
+          );
+          if (!toggle.classList.contains("show")) {
+            getDropdown(toggle).toggle();
+          }
+        });
+        dd.addEventListener("mouseleave", function (e) {
+          let toggle = e.target.querySelector(
+            ':scope>[data-bs-toggle="dropdown"]'
+          );
+          if (toggle.classList.contains("show")) {
+            getDropdown(toggle).toggle();
+          }
+        });
+      });
+  })(bootstrap);
 });
