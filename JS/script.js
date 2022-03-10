5cbfd5b3c59626f4458d7e671099d9e6de52b83f
leenades <57545008+leenades@users.noreply.github.com>
2022-03-10 12:26:46 +0400

    cbd investr calculator

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,124 @@
 // AOS.init();
+window.onload=function() {
+  calculateCompoundIntrest();
+};
+function CompoundCalculation(pv,r,ap,cp,ma,mp,addpayStart)
+{
+    var j;
+    var dataYearly = [];
+    var prevv=pv;
+    var val=[prevv];
+    if( addpayStart==0 ) val[0]=prevv+ap;
+    for(var i=0; i<mp; i++) {
+        val[i]=prevv;
+        j=(i+1)/ma;
+        if( j==Math.round(j) && addpayStart==0 )
+        val[i]+=ap;
+        j=(i+1)/cp;
+        if( j==Math.round(j) )
+        val[i]=val[i]*(1+r/100.0);
+        j=(i+1)/ma;
+        if( j==Math.round(j) && addpayStart==1 )
+        val[i]+=ap;
+        prevv=val[i];
+    }
+
+    if(mp>30){
+        dataYearly = val.filter(function(v, index, arr) {
+            return (index+1) % 12 == 0;
+        });
+        dataYearly.unshift(pv);
+        return dataYearly
+    } else {
+        val.unshift(pv);
+        return val;
+    }
+}
+
+function calculateCompoundIntrest() {
+    // values from input
+    var preVal = document.getElementById("startingAmount").value;
+    var rate = document.getElementById("growth").value;
+    var additionalPayment = document.getElementById("additionalAmount").value;
+
+    var period = 30; //no of years 
+    var addpayStart = 0; //payment at the start of month
+    
+    //if form is empty or not added any value
+    if( preVal=='' ) preVal='1000';
+    if( additionalPayment=='' ) additionalPayment='0';
+    if( rate=='' ) rate='10';
+
+    //converting string values in number
+    var preVal = parseFloat(preVal);
+    var additionalPayment = parseFloat(additionalPayment);
+    if(preVal==0) preVal=additionalPayment;
+    rate = parseFloat(rate);
+    var compoundYearly = 1; //compound intrest yearly
+    var monthlyAddPayment = 12; // Adding additional fund Monthly
+    if( additionalPayment==0 ) monthlyAddPayment=1;
+    var periodYearly = 1; //Period 30 Years * 1
+    period /= periodYearly; // convert to years
+    var period1 = period*compoundYearly;
+    var period2 = period*monthlyAddPayment;
+    rate /= compoundYearly;
+    var ratio=monthlyAddPayment/compoundYearly;
+    var maxperiod = period1>period2?period1:period2;
+    var compoundPeriod=12/compoundYearly;
+    var monthlyAdditional=12/monthlyAddPayment;
+    if( (compoundPeriod>=monthlyAdditional) && ((compoundPeriod/monthlyAdditional)==Math.round(compoundPeriod/monthlyAdditional)) ) { compoundPeriod/=monthlyAdditional; monthlyAdditional=1; }
+    else if( (monthlyAdditional>compoundPeriod) && ((monthlyAdditional/compoundPeriod)==Math.round(monthlyAdditional/compoundPeriod)) ) { monthlyAdditional/=compoundPeriod; compoundPeriod=1; }
+    //call for CBD Data
+    var CBDValue = CompoundCalculation(preVal,rate,additionalPayment,compoundPeriod,monthlyAdditional,maxperiod,addpayStart);
+     //call for Saving Data
+    var savingAcc = CompoundCalculation(preVal,1,additionalPayment,compoundPeriod,monthlyAdditional,maxperiod,addpayStart);
+    
+    //final Values
+    var totalCBD= CBDValue[CBDValue.length-1];
+    var totalSaving = savingAcc[savingAcc.length-1];
+    
+    //conversion of value
+    totalCBD = totalCBD.toFixed(0);
+    totalSaving = totalSaving.toFixed(0)
+
+    //Adding $  value in value 
+    totalCBD = '$' +totalCBD;
+    totalSaving = '$' +totalSaving
+
+    // for adding commas in value
+    totalCBD = numberWithCommas(totalCBD);
+    totalSaving = numberWithCommas(totalSaving);
+
+    // assigning value on UI
+    $('#savingvalue').html(totalSaving);
+    $('#cbdValue').html(totalCBD);
+
+    CBDVal=CBDValue; savingAccountValue=savingAcc; pay=additionalPayment;
+}
+function numberWithCommas(x){
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+$('#additionalAmount').on('change', function(){
+  $('.monthlyinvest').html('$'+ numberWithCommas($('#additionalAmount').val()));
+});
+$('#growth').on('change', function(){
+    switch ($(this).val()) {
+        case '10':
+            $('.growthind').html("High Growth");
+          break;
+        case '8':
+            $('.growthind').html("Growth");
+          break;
+        case '6':
+            $('.growthind').html("Balanced");
+          break;
+        case '4':
+            $('.growthind').html("Conservative");
+    }
+});
+$('#startingAmount').on('change', function(){
+  $('.startingInvest').html('$' + numberWithCommas($('#startingAmount').val()));
+});
 
 $(document).ready(function () {
   $(".back-to-styles").on("click", function () {
